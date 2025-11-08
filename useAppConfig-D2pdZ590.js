f45e598c283bf3e838e960142cda411150159ff7
supmamamamma <supmamamamma@users.noreply.github.com>
2025-11-08 11:54:48 +0000

    Deploy from 63a3cda4f63bcf9b8bd755e63a578cfd55695d7c

---
+++ useAppConfig-D2pdZ590.js
@@ -0,0 +1,2 @@
+import{cG as s,ad as t}from"./index-Cpjf-JvT.js";import{E as a}from"./appDialogsManager-WIpOGx5R.js";const n=()=>{const{rootScope:e}=s(),r=a();return t(()=>{const o=e.managers.apiManager.getAppConfig();return r.collect(o),o})};export{n as u};
+//# sourceMappingURL=useAppConfig-D2pdZ590.js.map
